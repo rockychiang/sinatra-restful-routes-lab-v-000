140278e9df2f6a0e3eb54c0c4ca4c6963581a360
Rocky Chiang <rocky@chem.ucla.edu>
2018-04-07 23:49:27 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,7 +33,6 @@ class ApplicationController < Sinatra::Base
     @recipe.ingredients = params[:ingredients]
     @recipe.cook_time = params[:cook_time]
     @recipe.save
-    binding.pry
     redirect "/recipes/#{@recipe.id}"
   end
 
