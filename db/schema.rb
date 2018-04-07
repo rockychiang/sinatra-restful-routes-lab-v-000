78b44d97bb486a2e2b801e6adcd7745a30bdc60f
Rocky Chiang <rocky@chem.ucla.edu>
2018-04-07 22:56:19 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180407223833) do
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.string "ingredients"
+    t.string "cook_time"
+  end
 
 end
